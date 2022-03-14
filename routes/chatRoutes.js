c8d9712a236f1bc0b1eaae715678d7faaf6e1b38
Samruddhi-Kulkarni <samruddhikulkarni27@gmail.com>
2022-03-14 12:06:33 +0530

    register

---
+++ routes/chatRoutes.js
@@ -0,0 +1,18 @@
+/*const express = require("express");
+const router = express.Router();
+const {
+    accessChat,
+    fetchChats,
+    createGroupChat,
+
+    removeFromGroup,
+    addToGroup,
+    renameGroup,
+} = require("../controllers/chatControllers");
+
+router.route('/').post(protect, accesChat);
+router.route('/').get(protect, fetchChats);
+router.route('/group').post(protect, createGroupChats);
+router.route('/rename').put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/groupadd").put(protect, addToGroup);*/
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+// const chatRoutes = require("./routes/chatRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/api/user', userRoutes);
+// app.use('api/chat', chatRoutes);
+
+// const _dirname1 = path();
+// if (process.env.NODE_ENV === `production`) {
+
+//     app.use(express.static(path.join(_dirname1c)))
+
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('Api is running');
+//     });
+// }
+app.use(notFound);
+app.use(errorHandler);
+const PORT = process.env.PORT || 3000
+app.listen(3000, console.log('server started on 3000 ${PORT}'));
